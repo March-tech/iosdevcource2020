eab396c8cadba8064ee0d4bdcdb5cd2c502d771d
Marta <marta.makhnis@gmail.com>
2020-01-16 23:55:41 +0300

    add home work

--- Lesson2.playground/Contents.swift
+++ Lesson2.playground/Contents.swift
@@ -1,57 +1,18 @@
-//class Play{
-//    func playground() {
-//        integer = 10
-//    }
-//    var integer: Int = 0
-//}
-let constInteger: Int = 0
-constInteger
-//constInteger = 1 // Cannot assign to value: 'ConstInteger' is a 'let' constant
-let constMaxUInt: UInt = UInt.max// 0- 18446....
-constMaxUInt
-let newConst = constInteger
-newConst
-var varieble: Int = constInteger
-varieble = 55
-varieble
-var varieble2 = varieble + 1
-varieble2
-varieble - 10
-varieble
-var multiply = varieble2 * varieble
-var division = varieble/12
-var rest = varieble%12
-var double1: Double = 55
-var doubleDivision = double1/12
-var casted = Double(varieble)
-
-
-varieble == varieble2 //сравнение тру или фолс
-
-varieble >= varieble2
-varieble != varieble2 // не равно
-
-//bool
-var boolean1: Bool = true
-boolean1
-boolean1 == true
-boolean1 != true
-var opposite = !boolean1
-
-// string
-
-var string: String = "qwe1\n wff"
-
-var newLine = "\n"
-var string2 = "string"
-  
 
 //home work - lesson 2
 
-var tring: String = "qwe1\n wff"
+let Const1: Int = 10
+var NewVar: Int = 21
+var Result1 = Const1+NewVar
+var Result2 = Const1*NewVar
+var NewVar2: Double = 12
+var Bool1 = (Const1 == NewVar)
+Bool1
+var NewVar3: String = "this is new string"
 
-var ewLine = "\n"
-var tring2 = "string"
+
+
+
 
 
 
