a2a4cbc45036893a5932e1d6d9d2cdea55a6127c
Marta <marta.makhnis@gmail.com>
2020-01-17 13:15:44 +0300

    new changes

--- Lesson2.playground/Contents.swift
+++ Lesson2.playground/Contents.swift
@@ -5,7 +5,8 @@ let Const1: Int = 10
 var NewVar: Int = 21
 var Result1 = Const1+NewVar
 var Result2 = Const1*NewVar
-var NewVar2: Double = 12
+var NewVar2: Double = 12.000000111111111111
+var newVar3: Float = 12345.99
 var Bool1 = (Const1 == NewVar)
 Bool1
 var NewVar3: String = "this is new string"
