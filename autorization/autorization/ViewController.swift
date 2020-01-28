a7817fa58c14b189ccfab6e61d2680c164d56667
Marta <marta.makhnis@gmail.com>
2020-01-28 20:35:15 +0300

    new

---
+++ autorization/autorization/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autorization
+//
+//  Created by Marta on 1/28/20.
+//  Copyright © 2020 Marta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
